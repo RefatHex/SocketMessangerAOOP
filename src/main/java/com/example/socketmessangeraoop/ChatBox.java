220e0a12d43f4e6358c3631bf2c8a952aa230271
refat <khondakar00@gmail.com>
2023-12-04 09:36:34 -0700

    Implemented private chat feature

--- src/main/java/com/example/socketmessangeraoop/ChatBox.java
+++ src/main/java/com/example/socketmessangeraoop/ChatBox.java
@@ -1,6 +1,5 @@
 package com.example.socketmessangeraoop;
 
-
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -30,16 +29,13 @@ public class ChatBox implements Initializable {
     @FXML
     private ScrollPane sp_main;
     private Client client;
-    private String username = "nabil";
+    private String username = "ras";
     private String targetUsername;
     @FXML
     private Button backBtn;
 
     public void setData(String targetUsername) {
         this.targetUsername = targetUsername;
-//        if (client == null) {
-//            initializeClient();
-//        }
         initializeClient();
     }
 
@@ -66,7 +62,7 @@ public class ChatBox implements Initializable {
 
     private void initializeClient() {
         try {
-            client = new Client(new Socket("localhost", 5555), username, targetUsername,this);
+            client = new Client(new Socket("localhost", 5555), username, targetUsername, this);
             System.out.println("Connected to server");
             client.listenForMessage();
         } catch (IOException e) {
@@ -79,33 +75,28 @@ public class ChatBox implements Initializable {
     }
 
     public void receiveMessageFromServer(String message) {
-        // Assuming the message format is "SenderUsername: Message"
-//        String[] parts = message.split(": ");
-
-//            String senderUsername = parts[0];
-//            String messageContent = parts[2];
-
-            addLabel( message);
-
+        addLabel(message);
     }
 
     // Add a message to the UI
     private void addMessageToUI(String message, boolean isSentByMe) {
-        HBox hBox = new HBox();
-        hBox.setAlignment(isSentByMe ? Pos.CENTER_RIGHT : Pos.CENTER_LEFT);
-        hBox.setPadding(new Insets(5, 5, 5, 10));
+        Platform.runLater(() -> {
+            HBox hBox = new HBox();
+            hBox.setAlignment(isSentByMe ? Pos.CENTER_RIGHT : Pos.CENTER_LEFT);
+            hBox.setPadding(new Insets(5, 5, 5, 10));
 
-        Text text = new Text(message);
-        TextFlow textFlow = new TextFlow(text);
-        textFlow.setStyle("-fx-color: rgb(239,242,255); " +
-                "-fx-background-color: " + (isSentByMe ? "rgb(15,125,242)" : "rgb(233,233,235)") +
-                ";-fx-background-radius: 20px;");
+            Text text = new Text(message);
+            TextFlow textFlow = new TextFlow(text);
+            textFlow.setStyle("-fx-color: rgb(239,242,255); " +
+                    "-fx-background-color: " + (isSentByMe ? "rgb(15,125,242)" : "rgb(233,233,235)") +
+                    ";-fx-background-radius: 20px;");
 
-        textFlow.setPadding(new Insets(5, 10, 5, 10));
-        text.setFill(isSentByMe ? Color.color(0.934, 0.945, 0.996) : Color.BLACK);
+            textFlow.setPadding(new Insets(5, 10, 5, 10));
+            text.setFill(isSentByMe ? Color.color(0.934, 0.945, 0.996) : Color.BLACK);
 
-        hBox.getChildren().add(textFlow);
-        vbox_messages.getChildren().add(hBox);
+            hBox.getChildren().add(textFlow);
+            vbox_messages.getChildren().add(hBox);
+        });
     }
 
     // Add this method to handle incoming messages and add them to the UI
--- src/main/java/com/example/socketmessangeraoop/Client.java
+++ src/main/java/com/example/socketmessangeraoop/Client.java
@@ -1,5 +1,6 @@
 package com.example.socketmessangeraoop;
 
+
 import java.io.*;
 import java.net.Socket;
 import java.sql.Connection;
@@ -7,27 +8,35 @@ import java.sql.PreparedStatement;
 import java.util.Scanner;
 
 public class Client {
+
     private Socket socket;
     private BufferedReader bufferedReader;
     private BufferedWriter bufferedWriter;
     private String username;
     private String targetUsername;
-    ChatBox chatBox;
+    private ChatBox chatBox;
 
-    public Client(Socket socket, String username, String targetUsername,ChatBox chatBox) {
+    public Client(Socket socket, String username, String targetUsername, ChatBox chatBox) {
         try {
             this.socket = socket;
             this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             this.username = username;
             this.targetUsername = targetUsername;
-            this.chatBox=chatBox;
+            this.chatBox = chatBox;
+
+            // Send the target username to the server
+            sendTargetUsername();
         } catch (IOException e) {
             closeAll(socket, bufferedReader, bufferedWriter);
         }
     }
 
-
+    private void sendTargetUsername() throws IOException {
+        bufferedWriter.write(targetUsername);
+        bufferedWriter.newLine();
+        bufferedWriter.flush();
+    }
 
     public void sendMessage(String message) {
         try {
@@ -42,19 +51,6 @@ public class Client {
             }
         } catch (IOException e) {
             closeAll(socket, bufferedReader, bufferedWriter);
-        }
-    }
-
-    private void getUserInput() {
-        try {
-            Scanner scanner = new Scanner(System.in);
-            while (socket.isConnected()) {
-                System.out.print("Enter your message: ");
-                String messageToSend = scanner.nextLine();
-                sendMessage(targetUsername + ": " + username + ": " + messageToSend);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
@@ -86,11 +82,12 @@ public class Client {
                         System.out.println("Disconnected from the server.");
                         break;
                     }
-//                     Check if the message is from the target username
-                    if (msgFromGChat.startsWith(targetUsername + ":")) {
+
+                    // Check if the message is from the target username
+//                    if (msgFromGChat.startsWith(targetUsername + ":")) {
                         System.out.println(msgFromGChat);
-                    this.chatBox.receiveMessageFromServer(msgFromGChat);
-                    }
+                        this.chatBox.receiveMessageFromServer(msgFromGChat);
+//                    }
                 } catch (IOException e) {
                     closeAll(socket, bufferedReader, bufferedWriter);
                 }
@@ -113,15 +110,4 @@ public class Client {
             e.printStackTrace();
         }
     }
-
-//    public static void main(String[] args) throws IOException {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Define your username: ");
-//        String targetUsername = "nabil";
-//        String username = scanner.nextLine();
-//        Socket socket = new Socket("localhost", 5555);
-//        Client client = new Client(socket, username, targetUsername);
-//        client.listenForMessage();
-//        new Thread(client::getUserInput).start();
-//    }
 }
